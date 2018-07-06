dc34194b1341fa8117bedd4a75e4ba1f3c4b227e
Bletsk <s.tomashevsky@oblakogroup.ru>
2018-07-06 18:44:47 +0300

    init

---
+++ inject.js
@@ -0,0 +1,25 @@
+/*var s = document.createElement('script');
+// TODO: add "script.js" to web_accessible_resources in manifest.json
+s.src = chrome.extension.getURL('script.js');
+s.onload = function() {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(s);*/
+
+var port = chrome.runtime.connect(chrome.runtime.id);
+
+port.onMessage.addListener(function(msg) {
+	window.postMessage(msg, '*');
+});
+
+window.addEventListener('message', function(event) {
+	// We only accept messages from ourselves
+	if (event.source != window) return;
+
+	if (event.data.type && ((event.data.type === 'SS_UI_REQUEST') ||
+							(event.data.type === 'SS_UI_CANCEL'))) {
+		port.postMessage(event.data);
+	}
+}, false);
+
+window.postMessage({ type: 'SS_PING', text: 'start' }, '*');
---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("Script is injected!");
+
+var userMedia;
+
+function getScreenShare(){
+	console.log("Inside getScreenShare");
+
+  var pending_request_id = chrome.desktopCapture.chooseDesktopMedia(["screen", "window"], onAccessApproved);
+
+	return navigator.webkitGetUserMedia({
+		audio: false,
+        video: {
+            mandatory: {
+	          	chromeMediaSource: 'desktop',
+	          	chromeMediaSourceId: 'irrelevant',
+	        	maxWidth: 1280,
+	           	maxHeight: 720,
+          	},
+          	optional: []
+       	}
+	});
+}
+
+function getAspectRatio(w, h) {
+    function gcd (a, b) {
+        return (b == 0) ? a : gcd (b, a%b);
+    }
+    var r = gcd (w, h);
+    return (w/r) / (h/r);
+}
